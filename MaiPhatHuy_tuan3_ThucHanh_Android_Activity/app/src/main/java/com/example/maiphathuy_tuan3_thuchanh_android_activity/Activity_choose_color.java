6cb63cc3e26c923363d408e7f74997e743f29e44
maihuy2882000 <maihuy2882000@gmail.com>
2021-04-07 22:06:09 +0700

    first commit

---
+++ MaiPhatHuy_tuan3_ThucHanh_Android_Activity/app/src/main/java/com/example/maiphathuy_tuan3_thuchanh_android_activity/Activity_choose_color.java
@@ -0,0 +1,14 @@
+package com.example.maiphathuy_tuan3_thuchanh_android_activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_choose_color extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_color);
+    }
+}
